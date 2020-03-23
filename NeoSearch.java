556d9bf64f7a344598b95b45406ded35b28fa3d2
Gabriela <zooiv3rde@gmail.com>
2020-03-23 13:55:13 -0300

    predicate, reduce and allMatch

---
+++ NeoSearch.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.function.Predicate;
+
+public class NeoSearch {
+
+    public static void main(String... manda) {
+        List<String> ls = List.of("Neo", "Morpheus", "Oracle", "Trinity", "Neo");
+
+        Predicate<String> neoSearch = str -> {
+            System.out.println("olhando pra Neo");
+            return str.contains("Neo");
+        };
+
+        var binaryNumbers = List.of(1, 0, 1, 1).stream();
+        Integer binarySum = binaryNumbers.reduce(Integer::sum).orElseThrow(StackOverflowError::new);
+
+        boolean neoFound = ls.stream().filter(str -> str.length() >= binarySum).allMatch(neoSearch);
+        System.out.println(neoFound);       
+    }
+}
